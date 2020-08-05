42475ec77ce9e44d3c694c8eafb62a32e8287b7a
David Y. Stephenson <david@davidystephenson.com>
2020-08-05 14:59:30 +0200

    Use fetch to convert title case

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 function main () {
   console.log('hello world!')
 
+  const base = 'https://brettterpstra.com/titlecase/?title='
+
   const form = document.querySelector('form')
   const input = document.querySelector('input')
   const preview = document.querySelector('#preview')
   const body = document.querySelector('body')
 
   // <input onChange={} />
-  input.addEventListener('input', (event) => {
-    console.log('value test:', event.target.value)
+  input.addEventListener('input', async (event) => {
+    try {
+      // 1. Encode the value to make it ready for URLs
+      const parameter = encodeURIComponent(event.target.value)
 
-    const uppercase = event.target.value.toUpperCase()
+      // 2. Combine the base url the encoded parameter
+      const url = `${base}${parameter}`
 
-    console.log('uppercase test:', uppercase)
+      // 3. Fetch the url - no options required, but the server must
+      // turn on CORS! This is always true in the browser, for both
+      // Axios and fetch. NOS did not turn on CORS, so it's impossible
+      // to request their website from the browser.
+      const response = await fetch(url)
 
-    preview.textContent = uppercase
+      // 4. Convert the response into a string or data
+      // const data = response.json() // JS data
+      const converted = await response.text() // Just a string
+
+      // 5. Connect the response to your page
+      preview.textContent = converted
+    } catch (error) {
+      throw error
+    }
   })
 
   // <form onSubmit={}>
